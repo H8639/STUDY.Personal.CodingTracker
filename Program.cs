4515ab83c3f412d4b85a6c3cd6b4f870187b3455
harsh <harsh@HARSHITHA>
2024-04-12 14:53:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day46Concepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
